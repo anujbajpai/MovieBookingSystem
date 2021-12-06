586b2b446ea82817476772c26ebd99fa0997a23a
anujbajpai <anuj.bajpai777@gmail.com>
2021-12-06 14:28:34 +0530

    Create Enums.cpp

---
+++ model/Enums.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+enum Language = {HINDI, ENGLISH, BENGALI, MARATHI, TELEGU, TAMIL, GUJRATI};
+enum Genre = {ACTION, COMEDY, FICTION, BIOGRAPHY, WAR};
+enum SeatType = {VIP, DELUXE, BALCONY};
+enum SeatStatus = {AVAILAIBLE}, ;
