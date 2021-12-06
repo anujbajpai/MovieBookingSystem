5bb7454a302504531d0623f039605c4851558705
anujbajpai <anuj.bajpai777@gmail.com>
2021-12-06 17:11:14 +0530

    Update Enums.cpp

--- model/Enums.cpp
+++ model/Enums.cpp
@@ -5,4 +5,6 @@ using namespace std;
 enum Language = {HINDI, ENGLISH, BENGALI, MARATHI, TELEGU, TAMIL, GUJRATI};
 enum Genre = {ACTION, COMEDY, FICTION, BIOGRAPHY, WAR};
 enum SeatType = {VIP, DELUXE, BALCONY};
-enum SeatStatus = {AVAILAIBLE}, ;
+enum SeatStatus = {AVAILAIBLE, OCCUPIED};
+enum PaymentMode = (CREDIT, DEBIT, CASH, NETBANKING, UPI);
+enum PaymentStatus = (SUCCESS, FAILED, PENDING);
